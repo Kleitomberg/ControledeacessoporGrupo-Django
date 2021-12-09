52bd1ef08eca305f2e7d3ed9717870efffc51991
kleitomberg <ozkberguinho@gmail.com>
2021-12-09 01:41:37 -0300

    perfil do usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from django.contrib.auth import views as auth_views 
+from .views import CreateUsuario, PerfilUpdate
 
 urlpatterns = [
 
@@ -8,5 +9,7 @@ path('login/',auth_views.LoginView.as_view(
     template_name='usuarios/login.html'
 ), name='login'),
 path('logout/',auth_views.LogoutView .as_view(), name='logout'),
+path('cadastro/',CreateUsuario.as_view(), name='cadastro'),
+path('atualizar-dados/',PerfilUpdate.as_view(), name='atualizar-dados'),
     
 ]
